c776ec049865909218c45a9697a3f97916cfbb6d
ramon-passos <ramonpassos2011@gmail.com>
2023-09-24 19:46:42 -0300

    add folders to each version

---
+++ trabalho-1/openMP/common.h
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#define DIMENSION 2048
+
+typedef struct {
+  int number;
+  float avg;
+} Neighbors;
+
+// Funcao que inicializa o primeiro
+// tabuleiro do jogo.
+void initializeGrid(float **grid) {
+  for (int i = 0; i < DIMENSION; i++) {
+    for (int j = 0; j < DIMENSION; j++) {
+      grid[i][j] = 0.0;
+    }
+  }
+  int lin = 1, col = 1;
+  grid[lin][col + 1] = 1.0;
+  grid[lin + 1][col + 2] = 1.0;
+  grid[lin + 2][col] = 1.0;
+  grid[lin + 2][col + 1] = 1.0;
+  grid[lin + 2][col + 2] = 1.0;
+  lin = 10;
+  col = 30;
+  grid[lin][col + 1] = 1.0;
+  grid[lin][col + 2] = 1.0;
+  grid[lin + 1][col] = 1.0;
+  grid[lin + 1][col + 1] = 1.0;
+  grid[lin + 2][col + 1] = 1.0;
+}
+
+// Funcao para printar o tabuleiro.
+void printGrid(float **grid) {
+  for (int i = 0; i < DIMENSION; i++) {
+    for (int j = 0; j < DIMENSION; j++) {
+      if (grid[i][j] > 0.0 && grid[i][j] <= 0.25)
+        printf("*");
+      else if (grid[i][j] > 0.25 && grid[i][j] <= 0.5)
+        printf("#");
+      else if (grid[i][j] > 0.5 && grid[i][j] <= 0.75)
+        printf("%");
+      else if (grid[i][j] > 0.75)
+        printf("@");
+      else
+        printf("_");
+    }
+    printf("\n");
+  }
+  printf("=======================================================\n");
+}
+
+// Funcao que retorna o numero de vizinhos
+// de uma celula.
+
+Neighbors getNeighbors(float **grid, int i, int j) {
+  int neighborsNum = 0;
+  float neighborsSum = 0;
+  int x, y;
+  int neighbors[8][2] = {{-1, 0},  {1, 0},  {0, -1}, {0, 1},
+                         {-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
+  Neighbors result;
+
+  // verifica horizontal e vertical
+  for (int k = 0; k < 8; k++) {
+    x = i + neighbors[k][0];
+    y = j + neighbors[k][1];
+    if (x < 0) {
+      x = DIMENSION - 1;
+    } else if (x >= DIMENSION) {
+      x = 0;
+    }
+    if (y < 0) {
+      y = DIMENSION - 1;
+    } else if (y >= DIMENSION) {
+      y = 0;
+    }
+    
+    if (grid[x][y] > 0) {
+      neighborsNum++;
+    }
+    neighborsSum += grid[x][y];
+  }
+
+  float avg = neighborsSum / 8.0;
+
+  result.number = neighborsNum;
+  result.avg = avg;
+  return result;
+}
+
+void assignCellValue(float **readingGrid, float **writingGrid, int i, int j) {
+  Neighbors neighbors = getNeighbors(readingGrid, i, j);
+  int numNeighbors = neighbors.number;
+  float avg = neighbors.avg;
+  float currentCell = readingGrid[i][j];
+  int alive = (currentCell > 0) ? 1 : 0;
+  
+  // Regra 3
+  writingGrid[i][j] = 0.0;
+  
+  // Regra 1
+  if ((numNeighbors == 2 || numNeighbors == 3) && alive) {
+    writingGrid[i][j] = currentCell;
+  }
+
+  // Regra 2
+  if (numNeighbors == 3 && !alive) {
+    writingGrid[i][j] = 1.0;
+  }
+}
