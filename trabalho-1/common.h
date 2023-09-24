62148d8770cc51a8ae032f28b3f633398b497349
Gabriel Schrader Vilas Boas <71721972+gaschrader@users.noreply.github.com>
2023-09-24 19:42:38 -0300

    working version

--- trabalho-1/common.h
+++ trabalho-1/common.h
@@ -1,20 +1,16 @@
 #include <stdio.h>
 #define DIMENSION 2048
 
-typedef struct
-{
+typedef struct {
   int number;
   float avg;
 } Neighbors;
 
 // Funcao que inicializa o primeiro
 // tabuleiro do jogo.
-void initializeGrid(float **grid)
-{
-  for (int i = 0; i < DIMENSION; i++)
-  {
-    for (int j = 0; j < DIMENSION; j++)
-    {
+void initializeGrid(float **grid) {
+  for (int i = 0; i < DIMENSION; i++) {
+    for (int j = 0; j < DIMENSION; j++) {
       grid[i][j] = 0.0;
     }
   }
@@ -34,12 +30,9 @@ void initializeGrid(float **grid)
 }
 
 // Funcao para printar o tabuleiro.
-void printGrid(float **grid)
-{
-  for (int i = 0; i < DIMENSION; i++)
-  {
-    for (int j = 0; j < DIMENSION; j++)
-    {
+void printGrid(float **grid) {
+  for (int i = 0; i < DIMENSION; i++) {
+    for (int j = 0; j < DIMENSION; j++) {
       if (grid[i][j] > 0.0 && grid[i][j] <= 0.25)
         printf("*");
       else if (grid[i][j] > 0.25 && grid[i][j] <= 0.5)
@@ -59,38 +52,30 @@ void printGrid(float **grid)
 // Funcao que retorna o numero de vizinhos
 // de uma celula.
 
-Neighbors getNeighbors(float **grid, int i, int j)
-{
+Neighbors getNeighbors(float **grid, int i, int j) {
   int neighborsNum = 0;
   float neighborsSum = 0;
   int x, y;
-  int neighbors[8][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
+  int neighbors[8][2] = {{-1, 0},  {1, 0},  {0, -1}, {0, 1},
+                         {-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
   Neighbors result;
 
   // verifica horizontal e vertical
-  for (int k = 0; k < 8; k++)
-  {
+  for (int k = 0; k < 8; k++) {
     x = i + neighbors[k][0];
     y = j + neighbors[k][1];
-    if (x < 0)
-    {
+    if (x < 0) {
       x = DIMENSION - 1;
-    }
-    else if (x >= DIMENSION)
-    {
+    } else if (x >= DIMENSION) {
       x = 0;
     }
-    if (y < 0)
-    {
+    if (y < 0) {
       y = DIMENSION - 1;
-    }
-    else if (y >= DIMENSION)
-    {
+    } else if (y >= DIMENSION) {
       y = 0;
     }
-
-    if (grid[x][y] > 0)
-    {
+    
+    if (grid[x][y] > 0) {
       neighborsNum++;
     }
     neighborsSum += grid[x][y];
@@ -103,26 +88,23 @@ Neighbors getNeighbors(float **grid, int i, int j)
   return result;
 }
 
-void assignCellValue(float **readingGrid, float **writingGrid, int i, int j)
-{
+void assignCellValue(float **readingGrid, float **writingGrid, int i, int j) {
   Neighbors neighbors = getNeighbors(readingGrid, i, j);
   int numNeighbors = neighbors.number;
   float avg = neighbors.avg;
   float currentCell = readingGrid[i][j];
   int alive = (currentCell > 0) ? 1 : 0;
-
+  
   // Regra 3
   writingGrid[i][j] = 0.0;
-
+  
   // Regra 1
-  if ((numNeighbors == 2 || numNeighbors == 3) && alive)
-  {
+  if ((numNeighbors == 2 || numNeighbors == 3) && alive) {
     writingGrid[i][j] = currentCell;
   }
 
   // Regra 2
-  if (numNeighbors == 3 && !alive)
-  {
+  if (numNeighbors == 3 && !alive) {
     writingGrid[i][j] = 1.0;
   }
 }
--- trabalho-1/main.c
+++ trabalho-1/main.c
@@ -1,13 +1,11 @@
 // Gabriel Schrader Vilas Boas, RA: 150981
-// João Pedro Assumpção Evaristo, RA:
-// Ramon da Silva Passos, RA:
+// João Pedro Assumpção Evaristo, RA: 147887
+// Ramon da Silva Passos, RA: 148011
 
 #include <stdio.h>
 #include <stdlib.h>
 #include "common.h"
-
-
-#define GENERATIONS 10
+#define GENERATIONS 2000
 
 
 // Funcao que retorna o numero de celulas
@@ -23,25 +21,30 @@ int getAliveCells(float **grid) {
   return sum;
 }
 
+// Funcao para trocar os grids
+void swap(float ***readingGrid, float ***writingGrid) {
+  float **temp = *readingGrid;
+  *readingGrid = *writingGrid;
+  *writingGrid = temp;
+}
+
 // Funcao que itera sobre as geracoes
 // e altera os tabuleiros.
 void play(float **readingGrid, float **writingGrid) {
   int aux = 0;
   while (aux < GENERATIONS) {
-    printf("geracao %d\n", aux);
-    printf("Numero celulas vivas: %d\n", getAliveCells(readingGrid));
-    printGrid(readingGrid);
+    printf("%d\n", aux);
     for (int i = 0; i < DIMENSION; i++) {
       for (int j = 0; j < DIMENSION; j++) {
         assignCellValue(readingGrid, writingGrid, i, j);
       }
     }
-    readingGrid = writingGrid;
+    swap(&readingGrid, &writingGrid);
     aux++;
   }
 }
 
-int main(void) {
+int main() {
   // readingGrid refere-se ao tabuleiro da
   // geracao atual
   float **readingGrid = (float **)malloc(DIMENSION * sizeof(float *));
@@ -58,6 +61,6 @@ int main(void) {
     writingGrid[i] = (float *)malloc(DIMENSION * sizeof(float));
   }
   play(readingGrid, writingGrid);
-  Neighbors teste = getNeighbors(readingGrid, 0, 0);
+  printf("Numero celulas vivas: %d\n", getAliveCells(readingGrid));
   return 0;
 }
