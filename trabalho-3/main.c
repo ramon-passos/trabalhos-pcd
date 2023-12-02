d7edd14710ee70b25dbb30a794292b54003355a0
ramon-passos <ramonpassos2011@gmail.com>
2023-12-02 17:21:11 -0300

    setup sendGrid and receiveGrid

--- trabalho-3/main.c
+++ trabalho-3/main.c
@@ -5,7 +5,6 @@
 // mpicc -o main main.c 
 // mpiexec -np 4 ./main
 
-
 #include <stdio.h>
 #include "mpi.h"
 #include "common.h"
@@ -16,32 +15,54 @@
 #define GENERATIONS 50
 #define DIMENSION 2048
 
-void workerProcess() {
+void sendGridToWorkers(float **readingGrid, int numProcesses) {
+    int i, j;
+    for (i = 1; i < numProcesses; i++) {
+        for (j = 0; j < DIMENSION; j++) {
+            MPI_Send(readingGrid[j], DIMENSION, MPI_FLOAT, i, 0, MPI_COMM_WORLD);
+        }
+    }
+}
 
+void receiveGridFromMaster(float **readingGrid) {
+    int i;
+    for (i = 0; i < DIMENSION; i++) {
+        MPI_Recv(readingGrid[i], DIMENSION, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    }
+}
+
+void workerProcess(int numProcesses) {
+    float **readingGrid = (float **)malloc(DIMENSION * sizeof(float *));
+    float **writingGrid = (float **)malloc(DIMENSION * sizeof(float *));
+
+    for (int i = 0; i < DIMENSION; i++) {
+        readingGrid[i] = (float *)malloc(DIMENSION * sizeof(float));
+    }
+
+    for (int i = 0; i < DIMENSION; i++) {
+        writingGrid[i] = (float *)malloc(DIMENSION * sizeof(float));
+    }
+
+    receiveGridFromMaster(readingGrid);
+    play(readingGrid, writingGrid);
+    printf("Numero celulas vivas: %d\n", getAliveCells(readingGrid));
 }
 
 void masterProcess(int numProcesses) {
-    // readingGrid refere-se ao tabuleiro da
-    // geracao atual
     float **readingGrid = (float **)malloc(DIMENSION * sizeof(float *));
-    for (int i = 0; i < DIMENSION; i++)
-    {
+    float **writingGrid = (float **)malloc(DIMENSION * sizeof(float *));
+
+    for (int i = 0; i < DIMENSION; i++) {
         readingGrid[i] = (float *)malloc(DIMENSION * sizeof(float));
+    }
+
+    for (int i = 0; i < DIMENSION; i++) {
+        writingGrid[i] = (float *)malloc(DIMENSION * sizeof(float));
     }
 
     fillZeros(readingGrid);
     initializeGrid(readingGrid);
-
-    // writingGrid refere-se ao tabuleiro da
-    // geracao futura
-    float **writingGrid = (float **)malloc(DIMENSION * sizeof(float *));
-    for (int i = 0; i < DIMENSION; i++)
-    {
-        writingGrid[i] = (float *)malloc(DIMENSION * sizeof(float));
-    }
-
-    play(readingGrid, writingGrid);
-    printf("Numero celulas vivas: %d\n", getAliveCells(readingGrid));
+    sendGridToWorkers(readingGrid, numProcesses);
 }
 
 int main(void) {
@@ -54,7 +75,7 @@ int main(void) {
     if(processId == 0) {
         masterProcess(numProcesses);
     } else {
-        workerProcess();
+        workerProcess(numProcesses);
     }
 
     MPI_Finalize();
